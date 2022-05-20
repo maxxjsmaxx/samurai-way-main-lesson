0846321c07b954637911a1895328a2c9a70a88a4
maxxjsmaxx <maxxjsmaxx@gmail.com>
2022-05-21 00:37:42 +0300

    lesson-001

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,42 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+     this is APP
+     <Rating />
+     <Accordion />
     </div>
   );
+}
+
+function Rating() {
+    return(
+        <div>
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+        </div>
+    )
+}
+
+function Star() {
+    return(
+        <div>star</div>
+    )
+}
+
+function Accordion() {
+    return(
+        <div>
+            <h3>Меню</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
