95e95aed7d88f5b80cb9f565a7dcb75a92b4393a
maxxjsmaxx <maxxjsmaxx@gmail.com>
2022-05-21 01:04:55 +0300

    lesson-002

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,56 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     this is APP
-     <Rating />
-     <Accordion />
-    </div>
-  );
+    return (
+        <div className="App">
+            <AppTitle />
+            <Rating/>
+            <Accordion/>
+            <Rating/>
+        </div>
+    );
+}
+
+function AppTitle() {
+    return <>this is APP component</>
 }
 
 function Rating() {
-    return(
+    return (
         <div>
-            <Star />
-            <Star />
-            <Star />
-            <Star />
-            <Star />
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
         </div>
     )
 }
 
 function Star() {
-    return(
+    return (
         <div>star</div>
     )
 }
 
 function Accordion() {
+    return (
+        <div>
+            <AccordionTitle />
+            <AccordionBody />
+        </div>
+    )
+}
+
+function AccordionTitle() {
+    return(
+        <div><h3>Меню</h3></div>
+    )
+}
+
+function AccordionBody() {
     return(
         <div>
-            <h3>Меню</h3>
             <ul>
                 <li>1</li>
                 <li>2</li>
